b7f8ada8134acefedf065d75332b41be732fcf4e
Taciana <alvestacyana37@gmail.com>
2023-05-31 20:36:19 -0300

    Exercicio 2

---
+++ 02-valorJantar.js
@@ -0,0 +1,9 @@
+
+let valorJantar = 80;
+let taxaGarcom = 10;
+let totalPagar = 0;
+
+total = 80 + ((taxaGarcom / 100) * 80);
+console.log(`valor do jantar: R$${valorJantar},00`);
+console.log(`valor taxa do Garcom: R$${(taxaGarcom / 100) * 80},00`);
+console.log(`Total a pagar: R$${total},00`);
